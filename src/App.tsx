7aa7c3254a8529b0d622804df5754d833049415c
Thomas Geissl <thomas.geissl@gmail.com>
2024-12-28 21:08:03 +0100

    added rajdhani font

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import './App.css'
+import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Rajdhani, Roboto, Arial, sans-serif", // Set your custom font here
+  },
+});
 
 function App() {
   return (
-    <>
-      <div>
-        coming soon...
-      </div>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* Optional: ensures consistent baseline CSS */}
+      <div>coming soon...</div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
